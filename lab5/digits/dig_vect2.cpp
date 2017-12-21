661c1d4c80879c36f6d2b32425c8747bdb06dc00
Ana Luisa Lamberto <alamber2@nd.edu>
2017-12-21 14:07:26 -0500

    Create dig_vect2.cpp

---
+++ lab5/digits/dig_vect2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//pass freq by reference so it changes without having to return a value
+void findfreq(vector<int> digits, vector<int>& freq);
+void results(vector<int> digits, vector<int> freq);
+
+int main()
+{
+        //array with random digits
+        vector<int> digits = {4,3,6,5,7,8,9,4,6,3,1,3,5,7,6,3,6,
+                  5,6,7,0,2,9,7,1,3,2,1,6,7,4,6,2,8,
+                  1,4,5,6,0,2,7,6,4,5,6,8,3,5,7,1,5};
+        vector<int> freq = {0,0,0,0,0,0,0,0,0,0};    // array to tally the frequency of digits
+
+        findfreq(digits, freq); // compute digits' frequencies
+        results(digits, freq);  // display frequencies for each digit
+
+        return 0;
+}
+
+void findfreq(vector<int> a, vector<int>& freq)
+{
+      for (auto it = a.begin(); it < a.end(); it++) //loop through entire vector checking for digit at each iteration
+                         (freq[*it])++; // add one to each position of freq that the position of a falls under
+}
+
+void results(vector<int> a, vector<int> freq)
+{
+        for (auto it = freq.begin(); it < freq.end(); it++)
+                cout <<  "digit " << it - freq.begin() << " occurs " << *it << " times" << endl; //it - freq.begin returns current position in vector
+}
