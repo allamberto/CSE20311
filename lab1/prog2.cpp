c8f8b0dc85844ccf36ae9d0671588a98cd70e000
Ana Luisa Lamberto <alamber2@nd.edu>
2017-12-21 13:56:02 -0500

    Create prog2.cpp

---
+++ lab1/prog2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int TD = 6;
+   int numTD;
+   cout << "How many touchdowns were scored during the game: ";
+   cin >> numTD;
+
+   int extra = 1;
+   int numExtra;
+   cout << "How many extra points were scored during the game: ";
+   cin >> numExtra;
+
+   int FG = 3;
+   int numFG;
+   cout << "How many field goals were scored during the game: ";
+   cin >> numFG;
+
+   int safeties = 2;
+   int numSafeties;
+   cout << "How many safeties were scored during the game: ";
+   cin >> numSafeties;
+
+   int totPoints = TD*numTD + extra*numExtra + FG*numFG + safeties*numSafeties;
+   cout << "The total number of points scored by the Irish was " << totPoints
+   <<"." << endl;
+
+   return 0;
+}
